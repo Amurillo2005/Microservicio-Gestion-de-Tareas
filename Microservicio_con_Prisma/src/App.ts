60dc01a732735387f1cf02595797bb4f95233495
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-03-27 01:23:22 -0500

    Se quitó la importación de cors en App.ts

--- Microservicio_con_Prisma/src/App.ts
+++ Microservicio_con_Prisma/src/App.ts
@@ -1,11 +1,9 @@
 import dotenv from "dotenv"
 import express from "express";
-import cors from "cors";
 import tareaRoutes from "./Routes/tareaRoutes"
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
