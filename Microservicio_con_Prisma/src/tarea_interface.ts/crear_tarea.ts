45a897c8918326ad6c75f47f8067af94d5951f98
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-09 14:01:46 -0500

    Se editó la interfaz CrearTarea

--- Microservicio_con_Prisma/src/tarea_interface.ts/crear_tarea.ts
+++ Microservicio_con_Prisma/src/tarea_interface.ts/crear_tarea.ts
@@ -1,7 +1,7 @@
 export interface CrearTarea {
     title: string;
-    description?: string;
-    status?: "PENDING" | "IN_PROGRESS" | "COMPLETED";
-    assignedTo?: string;
-    dueDate?: string;
+    description: string;
+    status: "PENDING" | "IN_PROGRESS" | "COMPLETED";
+    assignedTo: string;
+    dueDate: string;
 }
