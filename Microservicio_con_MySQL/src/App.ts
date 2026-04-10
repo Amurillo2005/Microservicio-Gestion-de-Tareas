dc8997e624926b1db5fb4038a2349fc1cda4c10f
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-10 13:51:11 -0500

    Inicialización del servidor

---
+++ Microservicio_con_MySQL/src/App.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
+})
