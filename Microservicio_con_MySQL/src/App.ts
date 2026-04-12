25a1b666765b165ab97d7dc5f0b2bbf4ba8d0d55
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-12 02:07:57 -0500

    Creación de la conexión de la base de datos

--- Microservicio_con_MySQL/src/App.ts
+++ Microservicio_con_MySQL/src/App.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { pool } from "./db/db.js";
 
 dotenv.config();
 const app = express();
@@ -9,3 +10,15 @@ app.use(express.json())
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 })
+
+async function conectarDB() {
+  try {
+    const connection = await pool.getConnection()
+    console.log('Conexión exitosa a la base de datos')
+    connection.release()
+  } catch (error) {
+    console.error('Error conectando a la base de datos:', error)
+  }
+}
+
+conectarDB()
