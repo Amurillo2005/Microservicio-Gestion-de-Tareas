3988f261f92c40818add47688ba2c504c0ef71c8
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-13 00:51:24 -0500

    Creación de la función controladora para actualizar una tarea completa

--- Microservicio_con_MySQL/src/Controllers/tareaController.ts
+++ Microservicio_con_MySQL/src/Controllers/tareaController.ts
@@ -46,3 +46,28 @@ export const obtenerTareaPorId = async (req: Request, res: Response): Promise<vo
         res.status(500).json({ error: "Error al obtener la tarea" });
     }
 }
+
+export const actualizarTareaCompleta = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const { title, description, status, assignedTo, dueDate } = req.body;
+        const tareaService = new TareaService();
+        const tarea = await tareaService.actualizarTareaCompleta(id as string, { title, description, status, assignedTo, dueDate });
+        res.status(200).json(tarea);
+    } catch (error: any) {
+        if (error.message === "No se encontró el id de esta tarea") {
+            res.status(404).json({
+                message: "Tarea no encontrada"
+            });
+            return;
+        }
+
+        if (error.message === "Todos los campos son obligatorios") {
+            res.status(400).json({
+                message: "Todos los campos son obligatorios"
+            });
+            return;
+        }
+        res.status(500).json({ error: "Error al actualizar la tarea" });
+    }
+}
