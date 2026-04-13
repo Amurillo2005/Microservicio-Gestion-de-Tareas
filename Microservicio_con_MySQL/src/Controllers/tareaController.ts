d8fb766e01ace7846a2ea29741cea15b0725f284
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-13 00:32:18 -0500

    Creación de la función controladora para obtener todas las tareas

--- Microservicio_con_MySQL/src/Controllers/tareaController.ts
+++ Microservicio_con_MySQL/src/Controllers/tareaController.ts
@@ -8,8 +8,24 @@ export const crearTarea = async (req: Request, res: Response): Promise<void> => 
         const tareaService = new TareaService();
         const tarea = await tareaService.crearTarea({ title, description, status, assignedTo, dueDate });
         res.status(201).json(tarea)
-    } catch (error) {
+    } catch (error: any) {
         console.error("Error al crear la tarea: ", error);
         res.status(500).json({ message: "Error al crear la tarea" });
+    }
+}
+
+export const obtenerTareas = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const tareaService = new TareaService();
+        const tareas = await tareaService.obtenerTodasLasTareas();
+        res.status(200).json(tareas);
+    } catch (error: any) {
+        if (error.message === "No se encontraron tareas") {
+            res.status(404).json({
+                message: "No se encontraron tareas"
+            });
+            return;
+        }
+        res.status(500).json({ error: "Error al obtener las tareas" });
     }
 }
