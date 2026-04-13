6b8eef0e5969a08f70c3d38189dd634c8b9cf1a7
Andrés Murillo <murilloandrescaleb@gmail.com>
2026-04-13 00:14:36 -0500

    Creación de la función para actualizar parcialmente una trea

--- Microservicio_con_MySQL/src/Services/tarea.service.ts
+++ Microservicio_con_MySQL/src/Services/tarea.service.ts
@@ -1,6 +1,6 @@
 import type { tarea } from "../tarea_interface/tarea.interface.js";
 import { pool } from "../db/db.js";
-import type { RowDataPacket } from "mysql2";
+import type { RowDataPacket, ResultSetHeader } from "mysql2";
 
 export class TareaService {
     async crearTarea(tarea: tarea){
@@ -8,10 +8,10 @@ export class TareaService {
             throw new Error("Todos los campos son obligatorios");
         }
 
-        const tareas = await pool.query(
+        const tareas = await pool.query<ResultSetHeader>(
             "INSERT INTO Tarea (title, description, status, assignedTo, dueDate) VALUES (?, ?, ?, ?, ?)",
             [tarea.title, tarea.description, tarea.status, tarea.assignedTo, tarea.dueDate]
-        )
+        );
 
         return tareas;
     }
@@ -41,12 +41,35 @@ export class TareaService {
             throw new Error("Todos los campos son obligatorios");
         }
 
-        const [tareas] = await pool.query(
+        const [tareas] = await pool.query<ResultSetHeader>(
             "UPDATE Tarea SET title = ?, description = ?, status = ?, assignedTo = ?, dueDate = ? WHERE id = ?",
             [tarea.title, tarea.description, tarea.status, tarea.assignedTo, tarea.dueDate, id]
         );
 
+        if (tareas.affectedRows === 0) {
+            throw new Error("No se encontró la tarea con el id especificado");
+        }
+
         return tareas;
     }
+
+    async actualizarTareaParcial(id: string, tarea: Partial<tarea>){
+        if (!id) {
+            throw new Error("No se encontró el id de esta tarea")
+        }
+
+        const [tareas] = await pool.query<ResultSetHeader>(
+            "UPDATE Tarea SET title = COALESCE(?, title), description = COALESCE(?, description), status = COALESCE(?, status), assignedTo = COALESCE(?, assignedTo), dueDate = COALESCE(?, dueDate) WHERE id = ?",
+            [tarea.title ?? null, tarea.description ?? null, tarea.status ?? null, tarea.assignedTo ?? null, tarea.dueDate ?? null, id]
+        );
+
+        if (tareas.affectedRows === 0) {
+            throw new Error("No se encontró la tarea con el id especificado");
+        }
+
+        return tareas;
+    }
+
+    
  
 }
